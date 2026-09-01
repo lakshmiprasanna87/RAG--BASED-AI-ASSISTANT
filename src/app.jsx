a5166940a80ede13c686356cafe5fd3ab26eb3d1
lakshmiprasanna87 <lakshmiprasannagajji@gmail.com>
2026-09-01 23:26:07 +0530

    Add files via upload

---
+++ src/app.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function App() {
+  return (
+    <div>
+      <h1>Multilingual IP RAG Assistant</h1>
+      <p>Frontend is working!</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/app.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div>
+      <h1>Multilingual IP RAG Assistant</h1>
+      <p>Frontend is working successfully.</p>
+    </div>
+  );
+}
+
+export default App;
